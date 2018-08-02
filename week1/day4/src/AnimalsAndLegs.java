1ba7b866435f0db211e78bf45983284a2b8e23dc
Mirjam Görög <mir.greek@gmail.com>
2018-08-02 16:58:32 +0200

    saving at 16:58

--- week1/day4/src/AnimalsAndLegs.java
+++ week1/day4/src/AnimalsAndLegs.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class AnimalsAndLegs {
+
+  public static void main(String[] args) {
+    // Write a program that asks for two integers
+    // The first represents the number of chickens the farmer has
+    // The second represents the number of pigs owned by the farmer
+    // It should print how many legs all the animals have
+    Scanner input1  = new Scanner(System.in);
+    Scanner input2  = new Scanner(System.in);
+    System.out.println("Hány csirke van a farmon?");
+    System.out.println("Hány malac van a farmon?");
+    int chicNum = input1.nextInt();
+    int pigNum = input2.nextInt();
+    System.out.println(chicNum*2 + pigNum*4);
+
+  }
 }
