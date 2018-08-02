3f911fd845de70dc7ddbb1b147a8f9b8be8784be
Mirjam Görög <mir.greek@gmail.com>
2018-08-02 15:53:01 +0200

    saving my work at 15:52

---
+++ week1/day4/src/BMI.java
@@ -0,0 +1,9 @@
+public class BMI {
+  public static void main(String[] args) {
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+  heightInM = heightInM * heightInM;
+    System.out.println(massInKg / heightInM);
+    // Print the Body mass index (BMI) based on these values
+  }
+}
