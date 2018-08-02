13fc0f78372a2dffd0fd368bf692269f53815dd0
Mirjam Görög <mir.greek@gmail.com>
2018-08-02 14:14:34 +0200

    first java todos done

---
+++ week1/day4/src/TwoNumbers.java
@@ -0,0 +1,29 @@
+import java.sql.SQLOutput;
+
+public class TwoNumbers {
+  public static void main(String[] args) {
+    int numOne = 22;
+        int numTwo = 13;
+    System.out.println(numOne + numTwo);
+    System.out.println(numOne - numTwo);
+    System.out.println(numOne * numTwo);
+    System.out.println(22 / 13.);
+    System.out.println(numOne / numTwo);
+    System.out.println(22 % 13);
+
+
+    // Create a program that prints a few operations on two numbers: 22 and 13
+
+    // Print the result of 13 added to 22
+
+    // Print the result of 13 substracted from 22
+
+    // Print the result of 22 multiplied by 13
+
+    // Print the result of 22 divided by 13 (as a decimal fraction)
+
+    // Print the integer part of 22 divided by 13
+
+    // Print the reminder of 22 divided by 13
+  }
+}
