98ab9fdb1e7738721572bd44aea428607c571ffd
Mirjam Görög <mir.greek@gmail.com>
2018-08-02 17:40:50 +0200

    Thursday's work finished

---
+++ week1/day4/src/IwontCheatOnTHeExams.java
@@ -0,0 +1,10 @@
+public class IwontCheatOnTHeExams {
+  public static void main(String[] args) {
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+    for (int i = 1; i < 101; i++);
+    
+    String myOutput = "I won't cheat on the exam!";
+    System.out.println(myOutput);
+  }
+}
