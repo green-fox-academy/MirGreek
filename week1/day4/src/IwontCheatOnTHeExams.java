b9be3724c06258db6371816df4c5ba4fe405b02f
Mirjam Görög <mir.greek@gmail.com>
2018-08-03 11:06:43 +0200

    I wont cheat on the  exercise solved

--- week1/day4/src/IwontCheatOnTHeExams.java
+++ week1/day4/src/IwontCheatOnTHeExams.java
@@ -2,9 +2,14 @@ public class IwontCheatOnTHeExams {
   public static void main(String[] args) {
 // Create a program that writes this line 100 times:
 // "I won't cheat on the exam!"
-    for (int i = 1; i < 101; i++);
-    
     String myOutput = "I won't cheat on the exam!";
-    System.out.println(myOutput);
+
+    for (int i = 1; i < 101; ++i) {
+      java.lang.String counter= i+ myOutput;
+          System.out.println(myOutput);
+        //PrintStream out = System.out;
+      //out.println("test")
+
+    }
   }
 }
