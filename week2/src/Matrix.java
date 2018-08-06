7020d8a5cb4762d06d395a1ab72dc70fde064f4f
Mirjam Görög <mir.greek@gmail.com>
2018-08-06 17:56:32 +0200

    Matrix not finished yet

---
+++ week2/src/Matrix.java
@@ -0,0 +1,28 @@
+public class Matrix {
+  public static void main(String[] args) {
+
+    int[][] matrix =new int[4][4];
+    for (int i = 0; i <matrix.length ; i++) {
+
+      for (int j = 0; j < matrix.length; j++) {
+        System.out.print(0);
+        matrix[i][j]={0};
+      }
+
+      System.out.println(0);
+    }
+  }
+  }
+
+//for (int k = 0; k <matrix.length; k++) {
+//          matrix[k][j] = 1;
+//        }
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
