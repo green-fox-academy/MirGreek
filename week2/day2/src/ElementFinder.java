04a213460dc70261b5ae78e9065088c70e7827dc
MirGreek <mir.greek@gmail.com>
2018-08-07 23:40:27 +0200

    Element finder add

--- week2/day2/src/ElementFinder.java
+++ week2/day2/src/ElementFinder.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ElementFinder {
-}
+
+  public static void main(String... args) {
+    ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    containsSeven(arrayList);
+      System.out.println(containsSeven(arrayList));
+    // Write a method that checks if the arrayList contains "7" if it contains return "Hoorray" otherwise return "Noooooo"
+    // The output should be: "Noooooo"
+    // Do this again with a different solution using different list methods!
+
+  }
+
+  public static String containsSeven(ArrayList nameOfArray) {
+   // for (int i = 0; i < nameOfArray.size(); i++) {
+    String itContains = "Hurray";
+     // nameOfArray.get(i);
+      if (nameOfArray.contains(7)) {
+        itContains = "Hurray";
+    } else itContains = "Noooo";
+
+      return itContains;
+  }
+
+
+  }
+
+
