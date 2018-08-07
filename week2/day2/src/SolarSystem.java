c1f567782df2421e827ff39240f7f04a18a344b0
MirGreek <mir.greek@gmail.com>
2018-08-07 15:12:32 +0200

    Saturn added to solarsystem

--- week2/day2/src/SolarSystem.java
+++ week2/day2/src/SolarSystem.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SolarSystem {
-}
+
+    public static void main(String... args){
+      ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+
+      // Saturn is missing from the planetList
+      // Insert it into the correct position
+      // Create a method called putSaturn() which has list parameter and returns the correct list.
+      planetList.add("Saturn");
+      System.out.println((planetList));
+      // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+    }
+  }
+
