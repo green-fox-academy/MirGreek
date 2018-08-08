9c78b9ad3192764ef9fa8035420c576aabce5d90
MirGreek <mir.greek@gmail.com>
2018-08-08 14:23:48 +0200

    Rect colored

--- week2/day3/src/Practice3.java
+++ week2/day3/src/Practice3.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
-public class Practice3 {
+public class Practice3 extends JPanel {
+  public void mainDraw(Graphics g) {
+
+    super.paintComponent(g);
+    this.setBackground(Color.LIGHT_GRAY);
+
+    g.setColor(Color.BLUE);
+    g.drawLine(1,50,2,3);
+
+
+
+  }
+
 }
+
+
