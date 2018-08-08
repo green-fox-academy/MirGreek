b7211767d7988513d1b9067d5e76fd6a637c6797
MirGreek <mir.greek@gmail.com>
2018-08-08 17:00:51 +0200

    Horizontal lines and square in center

--- week2/day3/src/HorizontalLines.java
+++ week2/day3/src/HorizontalLines.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class HorizontalLines {
-}
+
+    public static void mainDraw(Graphics graphics) {
+      // Create a line drawing function that takes 2 parameters:
+      // The x and y coordinates of the line's starting point
+      // and draws a 50 long horizontal line from that point.
+      // Draw 3 lines with that function. Use loop for that.
+      /*double yOld= 50+Math.random()*5;
+      double xOld= 50+Math.random()*5;
+      int x = (int) Math.round(xOld);
+      int y =(int)Math.round(yOld);*/
+      int x =10;
+      int y = 10;
+      int z = x + 50;
+      drawingLines(x, y, z, graphics);
+
+
+    }
+
+    public static void drawingLines(int x, int y,int z, Graphics graphics){
+      for (int i = 1; i <= 3 ; i++) {
+        graphics.drawLine(x,y * 10 * i, z ,y * 10 * i);
+
+      }
+
+
+    }
+
+
+
+
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+      JFrame jFrame = new JFrame("Drawing");
+      jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+      jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+      jFrame.add(new ImagePanel());
+      jFrame.setLocationRelativeTo(null);
+      jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+      @Override
+      protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        mainDraw(graphics);
+      }
+    }
+  }
+
