2e40352680c3b5fab3463a496ea1fa2405395f08
MirGreek <mir.greek@gmail.com>
2018-08-19 21:33:55 +0200

    add all previously modified files

--- week3/day3/FleetOfThings/src/Fleet.java
+++ week3/day3/FleetOfThings/src/Fleet.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+
 
 public class Fleet {
+  private ArrayList<Thing> things;
+
+  public Fleet() {
+    things = new ArrayList<>();
+  }
+
+  public void add(Thing thing) {
+    things.add(thing);
+  }
+
+    String milk;
+    String removeTheObs;
+    String standUp;
+    String eat;
+
+
+
+
+  @Override
+  public String toString() {
+    String result = "";
+    for(int i = 0; i < things.size(); i++) {
+      result += (i+1) + ". " + things.get(i) + "\n";
+    }
+    return result;
+  }
 }
--- week3/day3/FleetOfThings/src/Thing.java
+++ week3/day3/FleetOfThings/src/Thing.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Thing {
+  private String name;
+  private boolean completed;
+
+  public Thing(String name) {
+    this.name = name;
+  }
+
+  public void complete() {
+    this.completed = true;
+  }
+
+  @Override
+  public String toString() {
+    return (completed ? "[x] " : "[ ] ") + name;
+  }
 }
