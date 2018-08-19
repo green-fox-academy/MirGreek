f0764f40fa50d9762d95a132d39931a7a51eb11c
MirGreek <mir.greek@gmail.com>
2018-08-19 22:40:37 +0200

    Pirates can fight

--- week3/day7/src/Pirate.java
+++ week3/day7/src/Pirate.java
@@ -2,23 +2,49 @@ public class Pirate {
   public static void main(String[] args) {
     String answer = "Arghh, I'ma Pirate. How d'ya d'ink its goin?";
     String drink = "Pour me anudder!";
+    boolean pirateDies = false;
+    int chance = ((int)(Math.random() *10)/3);
+    System.out.println(chance);
+    System.out.println(howsItGoingMate(answer, drink,Boolean.parseBoolean("alive")));
 
-    System.out.println(howsItGoingMate(answer, drink));
+    brawl(chance, pirateDies);
   }
-  public static void drinkSomeRum(String drink2){
-
+  public static void drinkSomeRum(String drink2,boolean pirateDies){
+    if (pirateDies){
+      die();
+    }
     System.out.println(drink2);
+
   }
-  public static String howsItGoingMate(String answer,String drink){
+  public static String howsItGoingMate(String answer,String drink,boolean pirateDies){
     for (int i = 0; i <4 ; i++) {
-         drinkSomeRum(drink);
+      if (pirateDies){
+        die();
+      }
+         drinkSomeRum(drink, Boolean.parseBoolean("alive"));
+
         }
 
     return answer;
+
   }
   static void die(){
     System.out.println("He's dead");
   }
+
+  static void brawl(int chance, boolean pirateDies){
+
+    chance = ((int)(Math.random() *10)/3);
+    if (pirateDies){
+      die();
+    }
+      if (chance == 1){
+        System.out.println( "Pirate1 died :(");
+      } else if (chance == 2){
+        System.out.println("Pirate2 died :(");
+      } else
+      System.out.println("Both survived");
+    }
 }
 /*
 drinkSomeRum() - intoxicates the Pirate some
