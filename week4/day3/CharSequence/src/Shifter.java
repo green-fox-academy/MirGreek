454dfcf8b0ed964bfe63fd719d523947df8c22ce
MirGreek <mir.greek@gmail.com>
2018-08-22 15:44:23 +0200

    shifter finished

---
+++ week4/day3/CharSequence/src/Shifter.java
@@ -0,0 +1,23 @@
+public class Shifter implements CharSequence {
+  String string;
+  int number;
+  @Override
+  public int length() {
+    System.out.println(string.length()+ number);
+    return string.length();
+  }
+
+  @Override
+  public char charAt(int index) {
+    return string.charAt(index+number);
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    return string.subSequence(start+number,end+number);
+  }
+  public Shifter(String string, int number){
+    this.string = string;
+    this.number =number;
+  }
+}
