94b5b69a15576b89f94c7683bfb52ab6fc6815f8
MirGreek <mir.greek@gmail.com>
2018-08-23 17:24:28 +0200

    anagram

--- week4/day4/Anagram/src/Anagram.java
+++ week4/day4/Anagram/src/Anagram.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Anagram {
   public static void main(String[] args) {
@@ -6,16 +7,12 @@ public class Anagram {
     String string1 = "string";
     String string2 = "grints";
 
-    System.out.println(anagram());
+    System.out.println(anagram(string1, string2));
 
   }
-    public static boolean anagram(){
+    public static boolean anagram(String string1, String string2){
       ArrayList<Character> list = new ArrayList<>();
       ArrayList<Character> list2 = new ArrayList<>();
-
-      String string1 = "string";
-      String string2 = "grints";
-
 
       for (int i = 0; i < string1.length(); i++) {
          list.add(string1.charAt(i)) ;
@@ -24,10 +21,11 @@ public class Anagram {
       for (int j = 0; j < string2.length(); j++) {
         list2.add(string1.charAt(j)) ;
         }
+
          // if (list.containsAll(list2)){
            // System.out.println("anagram");
           //}else System.out.println("not anagram");
-            return list.containsAll(list2);
+            return (list.containsAll(list2) && list2.containsAll(list));
       }
 
     }
