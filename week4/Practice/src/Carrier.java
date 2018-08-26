b01f41e059bad096b282532f0decec9194a11d17
MirGreek <mir.greek@gmail.com>
2018-08-27 00:53:26 +0200

    aircraft almost good

--- week4/Practice/src/Carrier.java
+++ week4/Practice/src/Carrier.java
@@ -15,27 +15,36 @@ public class Carrier {
     int ammoStorage = 2000;
     int minimumAmmo=0;
       int filled = 0;
+      int filledPriority=0;
       int temp=0;
 
     for (int k = 0; k < airCrafts.size(); k++) {
-      minimumAmmo = +airCrafts.get(k).maxAmmo;
+      minimumAmmo +=airCrafts.get(k).maxAmmo;
     }
 
-    if (ammoStorage <minimumAmmo){
+    if (ammoStorage < minimumAmmo){
+      for (int x = 0; x <= airCrafts.size() ; x++) {
+        if (airCrafts.get(x).ammoStore < airCrafts.get(x).maxAmmo)
+          if (airCrafts.get(x).isPriority())
+            filledPriority+=x;
+        airCrafts.get(x).ammoStore=filledPriority;
+      }
 
     }else
-      for (int i = 0; i < airCrafts.size(); i++) {
-        if (airCrafts.get(i).ammoStore <= airCrafts.get(i).maxAmmo) {
-          for (int j = 0; j <= airCrafts.get(i).maxAmmo; j++) {
-            temp = j;
-            airCrafts.get(i).ammoStore=temp;
+      for (int i = 0; i <= airCrafts.size(); i++) {
+        if (airCrafts.get(i).ammoStore < airCrafts.get(i).maxAmmo) {
+          for (int j = 0; j < airCrafts.get(i).maxAmmo; j++) {
+            temp = i;
+            airCrafts.get(i).ammoStore+=temp;
           }
-        }
-        filled+=temp;
-      }
-      ammoStorage-=filled;
-    System.out.println(filled);
 
+          System.out.println(airCrafts.get(i).maxAmmo + " " +airCrafts.get(i).ammoStore);
+        }
+        //filled+=temp;
+      }
+      ammoStorage-=(temp+filledPriority);
+    System.out.println(ammoStorage);
+    System.out.println(temp);
     return ammoStorage;
   }
 
