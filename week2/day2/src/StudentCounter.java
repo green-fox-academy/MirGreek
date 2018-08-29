4a1ea74df82747a0b4a060af0b6215356db7f744
MirGreek <39343589+MirGreek@users.noreply.github.com>
2018-08-29 13:31:28 +0200

    Update StudentCounter.java

--- week2/day2/src/StudentCounter.java
+++ week2/day2/src/StudentCounter.java
@@ -2,3 +2,13 @@ package PACKAGE_NAME;
 
 public class StudentCounter {
 }
+int sumCounter = 0;
+for (int i = 0; i < 6; i++) {
+  Integer obj = (Integer)   map.get(i).get("candies");
+  int age = (int) map.get(i).get("age");
+ 
+  if (obj > 4) {
+  System.out.println(map.get(i).get("name"));
+  } else if (obj < 5) {
+    sumCounter = sumCounter + age;
+}
