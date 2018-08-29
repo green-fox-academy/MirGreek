29612cb4336a53e76d25848eeee425fbf54b6b99
MirGreek <mir.greek@gmail.com>
2018-08-29 17:33:51 +0200

    We get the most productive year

--- week5/LastYearExam2/src/MostProductiveYear.java
+++ week5/LastYearExam2/src/MostProductiveYear.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MostProductiveYear {
-  public static void main(String[] args)  {
+  public static void main(String[] args) {
     String studioName = "marvel";
     List<String> newLines = new ArrayList<>();
     List<String> myLines;
@@ -20,7 +20,8 @@ public class MostProductiveYear {
   public static void mostProductiveYear(String studioName) {
     List<String> myLines = new ArrayList<>();
     List<String> newLines = new ArrayList<>();
-    HashMap<Integer,Integer> yearCount = new HashMap<>();
+    HashMap<String, Integer> yearCount = new HashMap<>();
+    //HashMap<String, Integer> yearcCountNotEqual = new HashMap<>();
 
     String year = "";
     Path studio = Paths.get(studioName + ".txt");
@@ -35,34 +36,49 @@ public class MostProductiveYear {
     }
 
     System.out.println(newLines);
+    int sum = 0;
+
+
+    for (String yearNum : newLines)  {
+      if (!yearCount.containsKey(yearNum )){
+        yearCount.put(yearNum ,1);
+      } else{
+        Integer temp = yearCount.get(yearNum);
+        yearCount.put(yearNum, temp+1);
+      }
+
+      System.out.println(yearCount);
+    }
+
+
+    //System.out.println(yearCount);
 
   }
 
 }
 
+//if (Integer.parseInt(newLines.get(j))==Integer.parseInt(newLines.get(j)))
 
 
-
-
-  /**
-   * Write a method that helps you find the most productive year for a film studio.
-   * The method should take one argument, which is the name of the film studio.
-   * The method should try to open the related data file named `studioname`.csv,
-   * which is a comma separated file with 3 columns: name of the movie, release year, director
-   *
-   *  - If we do not have any data about the studio, the method should return an error message.
-   *  - If we have found data, the method should return an information message about the most productive year for that studio.
-   * RETURN: MOST PRODUCTIVE YEAR
-   * Examples:
-   *
-   * If we call the method with 'marvel':
-   * should return 'marvel has made the most movies in 2017.'
-   *
-   * If we call the method with 'ghibli':
-   * should print 'Cannot find studio, please try again later.'
-   *
-   * Hint:
-   *  - You can find some example files in this folder (marvel.csv and paramount.csv)
-   *  - Most productive year: The year in which the studio has made the most movies.
-   */
+/**
+ * Write a method that helps you find the most productive year for a film studio.
+ * The method should take one argument, which is the name of the film studio.
+ * The method should try to open the related data file named `studioname`.csv,
+ * which is a comma separated file with 3 columns: name of the movie, release year, director
+ * <p>
+ * - If we do not have any data about the studio, the method should return an error message.
+ * - If we have found data, the method should return an information message about the most productive year for that studio.
+ * RETURN: MOST PRODUCTIVE YEAR
+ * Examples:
+ * <p>
+ * If we call the method with 'marvel':
+ * should return 'marvel has made the most movies in 2017.'
+ * <p>
+ * If we call the method with 'ghibli':
+ * should print 'Cannot find studio, please try again later.'
+ * <p>
+ * Hint:
+ * - You can find some example files in this folder (marvel.csv and paramount.csv)
+ * - Most productive year: The year in which the studio has made the most movies.
+ */
 
