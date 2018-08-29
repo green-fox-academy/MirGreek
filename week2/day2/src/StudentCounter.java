4fdcf9afac45f73ee137c51f08767a86492f8243
MirGreek <39343589+MirGreek@users.noreply.github.com>
2018-08-29 13:32:30 +0200

    Update StudentCounter.java

--- week2/day2/src/StudentCounter.java
+++ week2/day2/src/StudentCounter.java
@@ -1,7 +1,49 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StudentCounter {
-}
+  public static void main(String[] args) {
+    ArrayList<Map<String, Object>> map = new ArrayList<Map<String, Object>>();
+
+    // Given this list of hashmaps...
+
+    Map<String, Object> row0 = new HashMap<String, Object>();
+    row0.put("name", "Theodor");
+    row0.put("age", 9.5);
+    row0.put("candies", 2);
+    map.add( row0 );
+
+    Map<String, Object> row1 = new HashMap<String, Object>();
+    row1.put("name", "Paul");
+    row1.put("age", 10);
+    row1.put("candies", 1);
+    map.add( row1 );
+
+    Map<String, Object> row2 = new HashMap<String, Object>();
+    row2.put("name", "Mark");
+    row2.put("age", 7);
+    row2.put("candies", 3);
+    map.add( row2 );
+
+    Map<String, Object> row3 = new HashMap<String, Object>();
+    row3.put("name", "Peter");
+    row3.put("age", 12);
+    row3.put("candies", 5);
+    map.add( row3 );
+
+    Map<String, Object> row4 = new HashMap<String, Object>();
+    row4.put("name", "Olaf");
+    row4.put("age", 12);
+    row4.put("candies", 7);
+    map.add( row4 );
+
+    Map<String, Object> row5 = new HashMap<String, Object>();
+    row5.put("name", "George");
+    row5.put("age", 3);
+    row5.put("candies", 2);
+    map.add( row5 );
+
 int sumCounter = 0;
 for (int i = 0; i < 6; i++) {
   Integer obj = (Integer)   map.get(i).get("candies");
