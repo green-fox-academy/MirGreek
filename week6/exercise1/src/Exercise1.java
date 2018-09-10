a45ef6e848affc5c24f6eef3ec5eed9589ace19c
MirGreek <mir.greek@gmail.com>
2018-09-10 16:12:09 +0200

    exercise 3 is done, positive numbers are squared

--- week6/exercise1/src/Exercise1.java
+++ week6/exercise1/src/Exercise1.java
@@ -5,12 +5,19 @@ public class Exercise1 {
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
-            numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+     numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println); //exercise1
 
-    System.out.println(numbers.stream()
+    System.out.println(numbers.stream()  //exercise2
         .filter(n -> n % 2 != 0)
         .mapToInt(n -> n)
         .average());
+
+
+    numbers.stream()
+        .filter(n -> n > 0)
+        .mapToDouble(n -> Math.pow(n,2))
+        .forEach(System.out::println);
+
 
 
 
