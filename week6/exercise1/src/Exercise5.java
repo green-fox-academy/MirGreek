62581c73c43fd7205e45357c5fc4703eb25cb807
MirGreek <mir.greek@gmail.com>
2018-09-10 16:51:53 +0200

    Uppercase characters filtered

---
+++ week6/exercise1/src/Exercise5.java
@@ -0,0 +1,16 @@
+public class Exercise5 {
+  public static void main(String[] args) {
+    String myString = "thisIsAString";
+
+    uppercase(myString);
+  }
+  //Write a Stream Expression to find the uppercase characters in a string!
+  private static String uppercase(String myString) {
+    myString.chars()
+        .filter(s -> Character.isUpperCase(s))
+        .mapToObj(s -> Character.toString((char) s))
+        .forEach(System.out::println);
+    return "";
+  }
+
+}
