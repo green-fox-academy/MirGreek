6f92d9256dbe4e843eb526154f11bce25b89f487
MirGreek <mir.greek@gmail.com>
2018-09-10 15:43:38 +0200

    Exercise2 added to exercise1

--- week6/exercise1/src/Exercise1.java
+++ week6/exercise1/src/Exercise1.java
@@ -5,8 +5,17 @@ public class Exercise1 {
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
-   // .filter(n -> n % 2 != 0)
-    numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+            numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+
+    System.out.println(numbers.stream()
+        .filter(n -> n % 2 != 0)
+        .mapToInt(n -> n)
+        .average());
+
+
+
+
+
 
   }
 
