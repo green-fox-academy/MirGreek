b7bcb753676c65ac045d95bda9f02f002e891b69
MirGreek <mir.greek@gmail.com>
2018-09-18 14:44:09 +0200

    Created table for accounts in Bankaccount of Symba

---
+++ week7/day2/bankofsymba/src/main/java/com/greenfoxacademy/bankofsymba/controller/BankAccountController.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.bankofsymba.controller;
+
+import com.greenfoxacademy.bankofsymba.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BankAccountController {
+  @RequestMapping(value = "/show")
+  public String createBankAccount(Model model){
+    BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
+    model.addAttribute("bankaccount",bankAccount);
+
+    return "bankaccount";
+  }
+}
