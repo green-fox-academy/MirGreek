8ec3c5936433c596d85b8a7ba28f81b2f7679cca
MirGreek <mir.greek@gmail.com>
2018-10-12 14:11:35 +0200

    authenticationmanager first tried

--- week10/practice/trybuiltinauthentication/src/main/java/com/greenfoxacademy/trybuiltinauthentication/configuration/FirstSecurityConfig.java
+++ week10/practice/trybuiltinauthentication/src/main/java/com/greenfoxacademy/trybuiltinauthentication/configuration/FirstSecurityConfig.java
@@ -1,12 +1,26 @@
 package com.greenfoxacademy.trybuiltinauthentication.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class FirstSecurityConfig extends WebSecurityConfigurerAdapter {
-
+  @Autowired
+  public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
+    auth
+        .inMemoryAuthentication()
+          .withUser("username")
+          .password("123")
+          .roles("USER")
+        .and()
+          .withUser("adminname")
+          .password("456")
+          .roles("ADMIN");
+  }
 
 }
--- week10/practice/trybuiltinauthentication/src/main/java/com/greenfoxacademy/trybuiltinauthentication/controller/AuthRestController.java
+++ week10/practice/trybuiltinauthentication/src/main/java/com/greenfoxacademy/trybuiltinauthentication/controller/AuthRestController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.trybuiltinauthentication.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,12 @@ public class AuthRestController {
   public String getLoginPage(){
     return "Login";
   }
-
+  @Secured("ROLE_USER")
   @RequestMapping("/stories")
   public String stories(){
     return "stories";
   }
-
+  @Secured("ROLE_ADMIN")
   @RequestMapping("/delete")
   public String delete(){
     return "delete";
