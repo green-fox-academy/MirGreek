f09434637bdc361ca392297f7cc22d0aaccd64e0
MirGreek <mir.greek@gmail.com>
2018-10-15 22:47:11 +0200

    created bean for encoder in main

--- week11/practiceSecurity/security/src/main/java/com/greenfoxacademy/security/SecurityApplication.java
+++ week11/practiceSecurity/security/src/main/java/com/greenfoxacademy/security/SecurityApplication.java
@@ -3,17 +3,18 @@ package com.greenfoxacademy.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class SecurityApplication {
 
-    @GetMapping("/")
-    public String sayHello(){
-      return "Hello";
-    }
-
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
   public static void main(String[] args) {
     SpringApplication.run(SecurityApplication.class, args);
   }
