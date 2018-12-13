4ed3d329b22509973001efbb3bf1ddb5cac51b6b
Mir <mir.greek@gmail.com>
2018-12-13 11:39:39 +0100

    practice : candles from hackerrank resolved

--- myOwnPractice/Hackerrank/src/Solution.java
+++ myOwnPractice/Hackerrank/src/Solution.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.math.*;
 import java.security.*;
 import java.text.*;
@@ -11,7 +12,7 @@ public class Solution {
     // Complete the birthdayCakeCandles function below.
     static int birthdayCakeCandles(int[] ar) {
 
-        ArrayList<Integer> newAr = new ArrayList<>();
+        List<Integer> newAr = new ArrayList<>();
         for (int i = 0; i < ar.length; i++) {
             newAr.add(ar[i]);
         }
@@ -19,19 +20,27 @@ public class Solution {
 
         int numberOfBigCandles = 0;
 
+        HashMap<Integer,Integer> countingMap = new HashMap<>();
+
         for (int i = 0; i < newAr.size(); i++) {
+            if (!countingMap.containsKey(newAr.get(i)) ) {
+                countingMap.put(newAr.get(i), 1);
+            } else countingMap.put(newAr.get(i), countingMap.get(newAr.get(i)) + 1);
+        }
 
+        List<Integer> list = new ArrayList<>(countingMap.values());
+            Collections.sort(list);
+        System.out.println(Collections.max(list));
 
-            }
-            return 0;
+        return Collections.max(list);
         }
 
 
 
 
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
+         Scanner scanner = new Scanner(System.in);
 
         int arCount = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -40,21 +49,15 @@ public class Solution {
 
         String[] arItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        char[] arItemsChar = arItems[0].toCharArray();
+        int[] array = new int[arCount];
+        for (int i = 0; i <arCount ; i++) {
+             array[i] = Character.digit(arItemsChar[i],arCount);
 
-        for (int i = 0; i < arCount; i++) {
-            int arItem = Integer.parseInt(arItems[i]);
-            ar[i] = arItem;
-            System.out.println(ar[i]);
         }
+        //int[] array = Arrays.stream(arItems).mapToInt(Integer::parseInt).toArray();
+        System.out.println(array[0]);
 
-        int result = birthdayCakeCandles(ar);
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+        birthdayCakeCandles(array);
     }
 }
